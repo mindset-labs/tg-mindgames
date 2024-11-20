88c8b810ae9f346b2039e2844fbb7e80186323d6
FaisalAl-Tameemi <faisal.al.tameemi@gmail.com>
2024-11-20 07:01:28 +0400

    setup contracts and game lifecycle package

---
+++ packages/cw-game-lifecycle/src/bin/schema.rs
@@ -0,0 +1,11 @@
+use cosmwasm_schema::write_api;
+
+use cw_game_lifecycle::msg::{ExecuteMsg, InstantiateMsg, QueryMsg};
+
+fn main() {
+    write_api! {
+        instantiate: InstantiateMsg,
+        execute: ExecuteMsg,
+        query: QueryMsg,
+    }
+}
