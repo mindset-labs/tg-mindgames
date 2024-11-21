67c3221084811090a756d7f35a28ceb161de6481
wanesurf <helwan.mande.1@ens.etsmtl.ca>
2024-11-21 03:12:50 -0500

    feat: implement create,join&start game

--- packages/cw-game-lifecycle/src/execute.rs
+++ packages/cw-game-lifecycle/src/execute.rs
@@ -1,21 +1,55 @@
 use crate::error::ContractError;
 use crate::state::GameConfig;
-use cosmwasm_std::{Addr, DepsMut, Env, Response, StdResult, Uint128};
+use crate::state::{Game, GameStatus, GAMES};
+use cosmwasm_std::{Addr, DepsMut, Env, MessageInfo, Response, Uint128};
 
-pub fn create_game(deps: DepsMut, config: GameConfig) -> Result<Response, ContractError> {
-    unimplemented!()
+pub fn create_game(
+    deps: DepsMut,
+    _env: Env,
+    info: MessageInfo,
+    config: GameConfig,
+) -> Result<Response, ContractError> {
+    let game_id = GAMES
+        .keys(deps.storage, None, None, cosmwasm_std::Order::Descending)
+        .next()
+        .map(|r| r.unwrap() + 1)
+        .unwrap_or(0);
+
+    let game = Game {
+        id: game_id,
+        players: vec![(info.sender, String::new(), Uint128::zero())],
+        rounds: vec![],
+        current_round: 0,
+        status: GameStatus::Created,
+        game_config: config,
+    };
+
+    GAMES.save(deps.storage, game_id, &game)?;
+
+    Ok(Response::new()
+        .add_attribute("action", "create_game")
+        .add_attribute("game_id", game_id.to_string()))
 }
 
 pub fn join_game(
     deps: DepsMut,
     game_id: u64,
+    _env: Env,
+    info: MessageInfo,
     telegram_id: String,
 ) -> Result<Response, ContractError> {
-    unimplemented!()
+    let mut game = GAMES.load(deps.storage, game_id)?;
+    game.players
+        .push((info.sender, telegram_id, Uint128::zero()));
+    GAMES.save(deps.storage, game_id, &game)?;
+    Ok(Response::new())
 }
 
 pub fn start_game(deps: DepsMut, game_id: u64) -> Result<Response, ContractError> {
-    unimplemented!()
+    let mut game = GAMES.load(deps.storage, game_id)?;
+    game.status = GameStatus::InProgress;
+    GAMES.save(deps.storage, game_id, &game)?;
+    Ok(Response::new())
 }
 
 pub fn commit_round(
