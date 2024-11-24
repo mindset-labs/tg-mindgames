601cf5421057b42a4682839193ffe5b375eba506
Mini <faisal.al.tameemi@gmail.com>
2024-11-24 15:57:31 +0400

    add a CW20 contract for the P2E token

--- contracts/cw-p2e/src/bin/schema.rs
+++ contracts/cw-p2e/src/bin/schema.rs
@@ -1,6 +1,6 @@
 use cosmwasm_schema::write_api;
 
-use cw20_play_to_earn::msg::{ExecuteMsg, InstantiateMsg, QueryMsg};
+use cw_p2e::msg::{ExecuteMsg, InstantiateMsg, QueryMsg};
 
 fn main() {
     write_api! {
