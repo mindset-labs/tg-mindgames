a6c5dfdf37444df6b20b6c6e38a0951f32b0869b
wanesurf <helwan.mande.1@ens.etsmtl.ca>
2024-11-30 01:16:34 -0500

    feat: add AbstraxionProvider

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,9 +2,18 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
+import { AbstraxionProvider } from "@burnt-labs/abstraxion";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
+    <AbstraxionProvider
+      config={{
+        contracts: [
+          "xion1z70cvc08qv5764zeg3dykcyymj5z6nu4sqr7x8vl4zjef2gyp69s9mmdka",
+        ],
+      }}
+    >
+      <App />
+    </AbstraxionProvider>
   </StrictMode>
 );
