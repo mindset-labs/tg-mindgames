30f08243e73a708ecf8dbb2ec4bad4432e1ac669
Mini <faisal.al.tameemi@gmail.com>
2024-12-01 22:06:44 +0400

    add artifacts

---
+++ scripts/optimize.sh
@@ -0,0 +1,11 @@
+
+#!/bin/bash
+
+echo "Optimizing contracts..."
+
+docker run --rm -v "$(pwd)":/code \
+  --mount type=volume,source="$(basename "$(pwd)")_cache",target=/target \
+  --mount type=volume,source=registry_cache,target=/usr/local/cargo/registry \
+  cosmwasm/optimizer:0.16.0
+
+echo "Optimization complete!"
