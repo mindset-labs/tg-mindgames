1775a7ada1580f81d4cadba80bae1408fa74e6a6
FaisalAl-Tameemi <faisal.al.tameemi@gmail.com>
2024-12-02 05:13:09 +0400

    extend cli.ts script to instantiate a p2e contract by code ID

--- cli.ts
+++ cli.ts
@@ -1,29 +1,44 @@
 import * as CWP2E from './codegen/CwP2e.types'
-import { CosmWasmClient } from '@cosmjs/cosmwasm-stargate'
+import dotenv from 'dotenv'
+import { CosmWasmClient, SigningCosmWasmClient } from '@cosmjs/cosmwasm-stargate'
+import { AccountData, coin, DirectSecp256k1Wallet } from "@cosmjs/proto-signing"
 
-const RPC_URL = 'https://rpc.xion-testnet-1.burnt.com:443'
-const P2E_CODE_ID = 1449
-const DILEMMA_CODE_ID = 1450
+dotenv.config()
+
+const RPC_URL = process.env.RPC_URL!
+const P2E_CODE_ID = parseInt(process.env.P2E_CODE_ID!)
+const P2E_CONTRACT_ADDRESS = process.env.P2E_CONTRACT_ADDRESS!
 
 async function cli() {
     const client = await CosmWasmClient.connect(RPC_URL)
+    const privateKey = Buffer.from(process.env.SIGNER_PRIVATE_KEY!, 'hex')
+    const wallet: DirectSecp256k1Wallet = await DirectSecp256k1Wallet.fromKey(privateKey, 'xion')
+    const accounts = await wallet.getAccounts()
+    const signingClient = await SigningCosmWasmClient.connectWithSigner(RPC_URL, wallet)
 
-    const codes = await client.getCodes()
-    console.log(codes)
+    // const codes = await client.getCodes()
+    // console.log(codes)
 
-    const p2eContract = await client.getCodeDetails(P2E_CODE_ID)
-    console.log(p2eContract)
+    // const p2eContract = await client.getCodeDetails(P2E_CODE_ID)
+    // console.log(p2eContract)
 
     const instantiateMsg: CWP2E.InstantiateMsg = {
         decimals: 2,
         initial_balances: [],
         initial_supply: '1000000000000',
         name: 'P2E Test',
-        symbol: 'P2E',
+        symbol: 'PTEX',
         reward_per_block: '10',
     }
 
+    const result = await signingClient.instantiate(accounts[0].address, P2E_CODE_ID, instantiateMsg, 'P2E Test', {
+        amount: [coin(0, 'uxion')],
+        gas: '200000',
+    })
+    console.log('Instantiate P2E Result:', result)
+
+    const balance = await signingClient.getBalance(accounts[0].address, 'uxion')
+    console.log('Remaining Account Balance:', balance)
 }
 
 cli()
-
