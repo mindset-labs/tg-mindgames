2234eb98b6e9b24e4620b63d03ca5847a3393898
wanesurf <helwan.mande.1@ens.etsmtl.ca>
2024-12-03 00:10:36 -0500

    feat: add contract address

--- frontend/src/screens/Settings/index.tsx
+++ frontend/src/screens/Settings/index.tsx
@@ -1,6 +1,8 @@
 import Navigation from "../../components/Navigation";
 
 export const Settings = () => {
+  //Counter contract address
+  //const counterContractAddress = 'xion1suthl30z5jg7r5pxm9e2srjtms2p0ev9yen709yvmpg4el4ckw0qmd4xfk';
   return (
     <div className="flex flex-col min-h-screen w-full bg-gradient-to-b from-[#160f28] to-black">
       <h1>Settings</h1>
