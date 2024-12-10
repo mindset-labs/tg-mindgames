1b5f5e2a208c8ecf318bb1acc86f0475aae7ecea
wanesurf <helwan.mande.1@ens.etsmtl.ca>
2024-12-10 01:03:52 -0500

    feat: add @twa-dev/sdk

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  // ... other config
+  theme: {
+    extend: {
+      animation: {
+        gradient: "gradient 15s ease infinite",
+      },
+      keyframes: {
+        gradient: {
+          "0%, 100%": {
+            "background-size": "200% 200%",
+            "background-position": "left center",
+          },
+          "50%": {
+            "background-size": "200% 200%",
+            "background-position": "right center",
+          },
+        },
+      },
+    },
+  },
+};
