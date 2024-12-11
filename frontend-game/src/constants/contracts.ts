77c6a32a5792a5926c347b53a26bbe59270cdedf
wanesurf <helwan.mande.1@ens.etsmtl.ca>
2024-12-10 20:34:49 -0500

    feat: add new contracts addresses

--- frontend-game/src/constants/contracts.ts
+++ frontend-game/src/constants/contracts.ts
@@ -1,9 +1,10 @@
 export const TREASURY = {
-    treasury: "xion1uf5yemc7tmw8luatfzcnw8lkueum5d4sltjj957qgjuancpgl82qru2sw2"
+    treasury: "xion1d67xkxskl3ncwp38ash7f2ex8cp56flxtmdzw3n0mw2l82v836wscn22lj"
 } as const;
 
 export const CONTRACTS = {
   counterContract: "xion1suthl30z5jg7r5pxm9e2srjtms2p0ev9yen709yvmpg4el4ckw0qmd4xfk",
   cwCooperationDilemma: "xion1lp7xue46k9909xycngp5ms459hsldc5cqqquqw0an0g4qnsahm4snyczyx",
   cwLifeCycle: "xion17ep30wmgw7xqefagdlx7kz3t746q9rj5xy37tf7g9v68d9d7ncaskl3qrz",
+  cwAsteroid: "xion1ejxq96lcngc5gzq6n5mdeqhh9efhfm33ju3zu8hrfs5pye8d7dfsfnqg9q",
 } as const;
