460ecd1a7da470f94299b240538f97ec3193357e
wanesurf <helwan.mande.1@ens.etsmtl.ca>
2024-12-12 19:39:44 -0500

    fix: add new contract addresses

--- frontend-game/src/constants/contracts.ts
+++ frontend-game/src/constants/contracts.ts
@@ -1,12 +1,12 @@
 export const TREASURY = {
-    treasury: "xion1d67xkxskl3ncwp38ash7f2ex8cp56flxtmdzw3n0mw2l82v836wscn22lj"
+    treasury: "xion1w4prpp4ytznnpzmxmgqydagdutj42k0vae4j32khm3k6hr9psy4qkhfp60"
 } as const;
 
 export const CONTRACTS = {
   counterContract: "xion1suthl30z5jg7r5pxm9e2srjtms2p0ev9yen709yvmpg4el4ckw0qmd4xfk",
   cwCooperationDilemma: "xion1lp7xue46k9909xycngp5ms459hsldc5cqqquqw0an0g4qnsahm4snyczyx",
   cwLifeCycle: "xion17ep30wmgw7xqefagdlx7kz3t746q9rj5xy37tf7g9v68d9d7ncaskl3qrz",
-  cwAsteroid: "xion1ejxq96lcngc5gzq6n5mdeqhh9efhfm33ju3zu8hrfs5pye8d7dfsfnqg9q",
+  cwAsteroid: "xion1fkpu38egjmdk2whhj8n5duc3m4p5csxgm46haeg5hsl89d2hws5smhctt9",
 } as const;
 
 // cosmwasm.wasm.v1.msgExecuteContract
