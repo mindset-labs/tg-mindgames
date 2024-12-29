98307fa29aa2f52f2104502b4071932e5ef71fd8
Mini <faisal.al.tameemi@gmail.com>
2024-12-30 01:37:57 +0400

    experiment with cw-orch

---
+++ contracts/cw-counter/src/interface.rs
@@ -0,0 +1,28 @@
+use cw_orch::{interface, prelude::*};
+
+use crate::msg::{ExecuteMsg, InstantiateMsg, MigrateMsg, QueryMsg};
+
+pub const CONTRACT_ID: &str = "counter";
+
+#[interface(InstantiateMsg, ExecuteMsg, QueryMsg, MigrateMsg, id = CONTRACT_ID)]
+pub struct CounterContract;
+
+impl<Chain> Uploadable for CounterContract<Chain> {
+    /// Return the path to the wasm file corresponding to the contract
+    fn wasm(_chain: &ChainInfoOwned) -> WasmPath {
+        artifacts_dir_from_workspace!()
+            .find_wasm_path("cw_counter")
+            .unwrap()
+    }
+    /// Returns a CosmWasm contract wrapper
+    fn wrapper() -> Box<dyn MockContract<Empty>> {
+        Box::new(
+            ContractWrapper::new_with_empty(
+                crate::contract::execute,
+                crate::contract::instantiate,
+                crate::contract::query,
+            )
+            // .with_migrate(crate::contract::migrate),
+        )
+    }
+}
--- contracts/cw-counter/src/msg.rs
+++ contracts/cw-counter/src/msg.rs
@@ -1,16 +1,29 @@
+use cw_orch::prelude::*;
+use cw_orch::{ExecuteFns, QueryFns};
+
 use cosmwasm_schema::{cw_serde, QueryResponses};
 
 #[cw_serde]
 pub struct InstantiateMsg {}
 
 #[cw_serde]
+#[derive(ExecuteFns)]
 pub enum ExecuteMsg {
     Increment {},
+    Reset { count: u64 },
 }
 
 #[cw_serde]
-#[derive(QueryResponses)]
+pub struct GetCountResponse {
+    pub count: u64,
+}
+
+#[cw_serde]
+#[derive(QueryFns, QueryResponses)]
 pub enum QueryMsg {
-    #[returns(u64)]
+    #[returns(GetCountResponse)]
     GetCount {},
 }
+
+#[cw_serde]
+pub enum MigrateMsg {}
